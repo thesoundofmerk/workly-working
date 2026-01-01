0a8ff2dcc25a2fe5f6887295b79884dbe3cbccc2
Max <thesoundofmerk@gmail.com>
2026-01-01 18:03:28 -0500

    Make AppComponent standalone and import RouterOutlet

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
-import { AuthService } from './services/auth.service';
-import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgIf, RouterOutlet, LoginComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  constructor(public auth: AuthService) {}
-}
+export class AppComponent {}
